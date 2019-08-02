c5f7b78ca49d5e505b120d1e38e41ce8526db320
GustavoM7 <gustavosmarquesf@gmail.com>
2019-08-02 15:07:52 -0300

    adaptando para execCommand

--- editor/src/components/Editor/Editor.js
+++ editor/src/components/Editor/Editor.js
@@ -3,78 +3,44 @@ import React, { Component } from 'react';
 class Editor extends Component {
 
     sandBox = () =>{
-        //console.log(window.getSelection().type);
 
-        let element = document.createElement("b");
-        element.innerHTML = " ";
+        let editor = document.getElementById("editor");
+        document.execCommand("heading", null, "h1");
 
-        if(window.getSelection().type === 'Caret'){
-            window.getSelection().getRangeAt(0).insertNode(element);
-        } else {
-            //window.getSelection().getRangeAt(0).surroundContents(element);
-            console.log(window.getSelection().getRangeAt(0).endContainer.parentNode.localName);
+        console.log(editor.innerHTML);
+        //console.log(window.getSelection().getRangeAt(0));
 
-        }
     }
 
-    format = (tag) =>{
-        /*
-            Criando tag de formatação
-            b: negrito
-            strong: realçado
-            i: itálico
-            em: ênfase
-            mark: marcado
-            small: pequeno
-            del: riscado
-            ins: sublinhado
-            sub: subscrito
-            sup: subrescrito
-            hx (1-4): título
-        */
-        let element = document.createElement(tag);
-        element.innerHTML = " ";
+    format = (commmand) =>{
+        document.execCommand(commmand);
+    }
 
-        //Intervalo ou ponto selecionado no editor:
-        let sel = window.getSelection();
-
-        //Chegando seleção é tipo cursor ou intervalo
-        if(sel.type === 'Caret'){
-            //Inserindo novo elemento dentro da área selecionada
-            window.getSelection().getRangeAt(0).insertNode(element);
-        } else {
-            //tipo intervalo
-            //Área selecionada já possui a formatação passada no parâmetro
-            if(sel.getRangeAt(0).endContainer.parentNode.localName === tag){
-                //Removendo formatação
-
-            } else {
-                //Inserindo tag em volta do intervalo selecionado para formatação.
-                sel.getRangeAt(0).surroundContents(element);
-            }
-            
-        }
-
+    titles = (width) =>{
+        document.execCommand("heading", null, width);
     }
 
     render(){
         return(
             <div>
                 <div id="toolbar">
-                    <button onClick={() => this.format("h2")}>titulo 1</button>
-                    <button onClick={() => this.format("h3")}>titulo 2</button>
-                    <button onClick={() => this.format("h4")}>titulo 3</button>
-                    <button onClick={() => this.format("b")}>negrito</button>
-                    <button onClick={() => this.format("i")}>italico</button>
-                    <button onClick={() => this.format("ins")}>sublinhado</button>
-                    <button>justificado</button>
-                    <button>esquerda</button>
-                    <button>central</button>
-                    <button>direita</button>
-                    <button>lista ordenada</button>
-                    <button>lista não ordenada</button>
+                    <button onClick={() => this.titles("h2")}>titulo 1</button>
+                    <button onClick={() => this.titles("h3")}>titulo 2</button>
+                    <button onClick={() => this.titles("h4")}>titulo 3</button>
+                    <button onClick={() => this.format("bold")}>negrito</button>
+                    <button onClick={() => this.format("italic")}>italico</button>
+                    <button onClick={() => this.format("underline")}>sublinhado</button>
+                    <button onClick={() => this.format("justifyFull")}>justificado</button>
+                    <button onClick={() => this.format("justifyLeft")}>esquerda</button>
+                    <button onClick={() => this.format("justifyCenter")}>central</button>
+                    <button onClick={() => this.format("justifyRight")}>direita</button>
+                    <button onClick={() => this.format("insertOrderedList")}>lista ordenada</button>
+                    <button onClick={() => this.format("insertUnorderedList")}>lista não ordenada</button>
                     <button>imagem</button>
                     <button>vídeo</button>
+                    <button>link</button>
+                    <button onClick={() => this.format("undo")}>desfazer</button>
+                    <button onClick={() => this.format("redo")}>refazer</button>
                 </div>
 
                 <div contentEditable="true" designmode="on" id="editor" spellCheck="true"> digite aqui... </div>
